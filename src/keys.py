3be40b015935b89f7c2bc8e009c56e84fe56035a
Phumin Maliwan <32133224+mantvmass@users.noreply.github.com>
2022-01-19 03:43:33 +0800

    Update keys.py

--- src/keys.py
+++ src/keys.py
@@ -1,2 +1,2 @@
-tokens_discord = "OTMyOTg2OTQ0Mzc0MzA4ODk0.Yea9zQ.KHRb7rtQvL7SmsnfnROmrYdNlxA"
-api_key_coinmarketcap = "7bc82129-376d-45d1-ba71-6940850c7579"
+tokens_discord = "your tokens"
+api_key_coinmarketcap = "your keys api"
